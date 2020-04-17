658be171422f5f7830b815ee83b925adb3a5b8d6
Alok Chaudhary <alokcdhy@gmail.com>
2020-04-17 09:53:13 +0545

    MODEL - relation defined on user and question model

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use  Illuminate\Support\Str;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -36,4 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    //    get user related questions
+    public function questions(){
+        return $this->hasMany(Question::class);
+    }
+    //    set slug attribute
+    public  function setTitleAttribute($value){
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=Str::slug($value);
+    }
 }
